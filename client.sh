285d3cfd154bd58d9445633f7c04cad7e653aea3
Truong Bui <btxtruong@gmail.com>
2024-03-14 23:32:20 +0700

    initial

---
+++ client.sh
@@ -0,0 +1,4 @@
+curl http://localhost:1555/block-odd &
+curl http://localhost:1555/block-even &
+
+wait
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+
+const app = express()
+
+async function asyncEven (i) {
+  console.log('async even', i)
+}
+
+async function asyncOdd (i) {
+  console.log('async odd', i)
+}
+
+function setTimeoutPromise (delay) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), delay)
+  })
+}
+
+function setImmediatePromise () {
+  return new Promise((resolve) => {
+    setImmediate(() => resolve())
+  })
+}
+
+function setTimeoutOnly (delay) {
+  setTimeout(() => {}, delay)
+}
+
+app.get('/block-odd', async function odd (req, res) {
+  for (let i = 1; i < 10; i += 2) {
+    // console.log('odd', i);
+    // await asyncOdd(i)
+    console.log('odd', i)
+    // await setTimeoutPromise(0)
+    // await setImmediatePromise();
+    setTimeoutOnly(0)
+  }
+  res.send('odd! ok')
+})
+
+app.get('/block-even', async function even (req, res) {
+  for (let i = 0; i < 10; i += 2) {
+    // console.log('even', i);
+    // await asyncEven(i)
+    console.log('even', i)
+    // await setTimeoutPromise(0)
+    // await setImmediatePromise();
+    setTimeoutOnly(0)
+
+  }
+  res.send('even! ok')
+})
+
+const PORT = process.env.PORT || 1555
+let server = app.listen(PORT, () => console.log('server listening on :' + PORT))
